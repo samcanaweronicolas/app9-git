ebda0b8115d8944e44127a2a606ec6c8fde37513
Lanix 1 <Lanix 1>
2018-05-30 12:06:44 -0500

    subida4

--- js/acciones.js
+++ js/acciones.js
@@ -21,6 +21,7 @@
 	 var options={
 		 enableHighAccuracy : true,
 		 maximumAge: 3600000
+		 
 	 }
 	 
 	 var watchID = navigator.geolocation.getCurrentPosition(onSuccess, onError, options);
@@ -34,6 +35,31 @@
 		 'Heading:'    +position.coords.heading    +'\n' +
 		 'Speed:'    +position.coords.speed    +'\n' +
 		 'Timestamp:'    +position.timestamp   +'\n');
+	 };
+	 function onError(error){
+		 alert('code:'  +error.code  + +'\n' + 'message:' + error.message +'\n');
+	 }
+ }
+ 
+ function watchPosition(){
+	 var options={
+		 maximumAge: 3600000,
+		 timeout: 3000,
+		 enableHighAccuracy : true,
+		 
+	 }
+	 
+	 var watchID = navigator.geolocation.getCurrentPosition(onSuccess, onError, options);
+	 function onSuccess(position){
+		 
+		 alert('latitud:').html(position.coords.latitude);
+		 $('#longitud:').html(position.coords.longitude);
+		 $('#altitud:').html(position.coords.altitude);
+		 $('#accuracy:').html(position.coords.accuracy);
+		 $('#aaccuracy:').html(position.coords.altitudeAccuracy);
+		 $('#headingg:').html(position.coords.heading);
+		 $('#speed:').html(position.coords.speed);
+		 $('#timestamp:').html(position.timestamp);
 	 };
 	 function onError(error){
 		 alert('code:'  +error.code  + +'\n' + 'message:' + error.message +'\n');
